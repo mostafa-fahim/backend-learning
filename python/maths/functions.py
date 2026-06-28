b7f688503935e99eae9714d9f6ee543cfd1e8113
mostafa-fahim <mostafafahim280@gmail.com>
2026-06-28 16:11:55 +0600

    Practice math functions

---
+++ python/maths/functions.py
@@ -0,0 +1,17 @@
+import math
+print(math.floor(2.3))
+print(math.floor(2.2))
+print(math.floor(5.4))
+print(math.floor(-1))
+print(math.floor(1.0))
+print(math.floor(2.2))
+
+print(math.ceil(2.1))
+print(math.ceil(0.0))
+
+# Built-in:
+print(abs(2.5))
+print(abs(-2.5))
+print(abs(-5))
+
+print(round(2.5))
