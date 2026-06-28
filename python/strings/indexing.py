4ad8324f9b3a1e4dcd32fd788327cc7e61c32865
mostafa-fahim <mostafafahim280@gmail.com>
2026-06-28 11:43:38 +0600

    Practice string indexing

---
+++ python/strings/indexing.py
@@ -0,0 +1,17 @@
+name = "Fahim"
+print(name[1])
+
+name = "Fahim"
+print(name[0])
+
+name = "Fahim"
+print(name[-1])
+
+name = "Fahim"
+print(name[2])
+
+name = "Fahim"
+print(name[-4])
+
+name = "Fahim"
+print(name[3])
