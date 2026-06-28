8f8eba2a541ac1fa763241fc8a608d67d237fad7
mostafa-fahim <mostafafahim280@gmail.com>
2026-06-28 11:53:14 +0600

    Practice string slicing

---
+++ python/strings/slicing.py
@@ -0,0 +1,17 @@
+name = "Python"
+print(name[1:3])
+
+name = "Python"
+print(name[0:4])
+
+name = "Python"
+print(name[2:-1])
+
+name = "Python"
+print(name[2:])
+
+name = "Python"
+print(name[:3])
+
+name = "Python"
+print(name[:])
