1fa1c59bdb2373833c229c91e6ba55d096d59b2b
mostafa-fahim <mostafafahim280@gmail.com>
2026-06-29 15:10:42 +0600

    Add 2d list

--- python/lists/list.py
+++ python/lists/list.py
@@ -8,3 +8,13 @@ print(names[3][2:])
 print(names)
 names[0] = "Don"
 print(names)
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(matrix[0][0])
+print(matrix[0][1])
+print(matrix[1][2])
+print(matrix[2][0])
