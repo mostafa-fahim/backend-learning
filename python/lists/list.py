110bc9f7e338fdf2b8278b7073e3c38a97d0de2b
mostafa-fahim <mostafafahim280@gmail.com>
2026-06-29 15:16:52 +0600

    Add 2d list iteration

--- python/lists/list.py
+++ python/lists/list.py
@@ -1,20 +1,24 @@
-names = ["John", "Larry", "Max", "Moon"]
-print(names[0][1:3])
-print(names[2])
-print(names[3])
-print(names[1][1:])
-print(names[-1])
-print(names[3][2:])
-print(names)
-names[0] = "Don"
-print(names)
+# names = ["John", "Larry", "Max", "Moon"]
+# print(names[0][1:3])
+# print(names[2])
+# print(names[3])
+# print(names[1][1:])
+# print(names[-1])
+# print(names[3][2:])
+# print(names)
+# names[0] = "Don"
+# print(names)
 
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
-print(matrix[0][0])
-print(matrix[0][1])
-print(matrix[1][2])
-print(matrix[2][0])
+# print(matrix[0][0])
+# print(matrix[0][1])
+# print(matrix[1][2])
+# print(matrix[2][0])
+ 
+for row in matrix:
+    for number in row:
+        print(number)
