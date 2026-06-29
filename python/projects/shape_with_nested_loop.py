b303cf79d1bc648851c80f50d7e46ec48a743faa
mostafa-fahim <mostafafahim280@gmail.com>
2026-06-29 12:47:02 +0600

    Build shapes with nested loop

---
+++ python/projects/shape_with_nested_loop.py
@@ -0,0 +1,15 @@
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    output = ""
+    for count in range(x_count):
+        output += "x"
+    print(output)
+
+print("-------")
+
+numbers = [2, 2, 2, 2, 6]
+for x_count in numbers:
+    output = ""
+    for count in range(x_count):
+        output += "x"
+    print(output)
