730613c7b292d2788e65fc940dc3952d93bf4ad2
mostafa-fahim <mostafafahim280@gmail.com>
2026-06-29 13:45:02 +0600

    Practice list

---
+++ python/lists/list.py
@@ -0,0 +1,10 @@
+names = ["John", "Larry", "Max", "Moon"]
+print(names[0][1:3])
+print(names[2])
+print(names[3])
+print(names[1][1:])
+print(names[-1])
+print(names[3][2:])
+print(names)
+names[0] = "Don"
+print(names)
