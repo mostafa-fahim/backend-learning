277d7565adc8cd680513918506c61aae052f69fd
mostafa-fahim <mostafafahim280@gmail.com>
2026-06-29 09:38:17 +0600

    Practice for loop

---
+++ python/loops/for.py
@@ -0,0 +1,23 @@
+for char in "Python":
+    print(char)
+
+for item in ["Max", "Dog", "Cat"]:
+    print(item)
+
+for number in [1, 2, 3, 4, 5, 6, 7]:
+    print(number)
+
+for number in range(5):
+    print(number)
+
+for number in range(1, 4):
+    print(number)
+
+for number in range(1, 6, 2):
+    print(number)
+
+for number in range(5, 1, -1):
+    print(number)
+
+for number in range(2, 10, 2*10):
+    print(number)
