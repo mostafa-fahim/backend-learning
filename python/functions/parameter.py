0841180d5650e7a74e6b9ce3b80e8895069c7540
mostafa-fahim <mostafafahim280@gmail.com>
2026-07-11 15:52:03 +0600

    Add double parameters

--- python/functions/parameter.py
+++ python/functions/parameter.py
@@ -4,3 +4,14 @@ def greet_user(name):
 
 
 greet_user("Lali")
+
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome to the team man")
+
+
+print("Start")
+greet_user("John", "Max")
+greet_user(first_name="Rick", last_name= "Mama")
+greet_user("Nano", last_name= "John")
+print("Finish")
