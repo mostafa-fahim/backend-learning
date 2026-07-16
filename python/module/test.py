29cf87bb3fe82ab5fa096926111af8d6be3c8a8a
mostafa-fahim <mostafafahim280@gmail.com>
2026-07-16 16:12:02 +0600

    Practice module import

---
+++ python/module/test.py
@@ -0,0 +1,21 @@
+import converters
+print(converters.kg_to_lbs(62))
+
+from converters import lbs_to_kg
+print(lbs_to_kg(137.78))
+
+
+
+
+import number_finder
+numbers = [2, 4, 6, 5]
+
+print(number_finder.find_max(numbers))
+
+
+
+
+from number_finder import find_max
+
+numbers = [2, 3, 7, 5, 6, 8, 5]
+print(find_max(numbers))
