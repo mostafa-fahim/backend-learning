b5d62ee01a5c980b7013396dc76b721367222828
mostafa-fahim <mostafafahim280@gmail.com>
2026-07-16 18:30:00 +0600

    Change the max variable into largest

--- python/module/number_finder.py
+++ python/module/number_finder.py
@@ -1,6 +1,6 @@
 def find_max(numbers):
-    max = numbers[0]
+    largest = numbers[0]
     for number in numbers:
-        if number > max:
-            max = number
-    return max
+        if number > largest:
+            largest = number
+    return largest
