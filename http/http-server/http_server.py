39eb5c851bb5b7921d355f2002c1b23a92f8f082
mostafa-fahim <mostafafahim280@gmail.com>
2026-07-25 09:57:57 +0600

    Set up basic HTTP server

---
+++ http/http-server/http_server.py
@@ -0,0 +1,11 @@
+# from http.server import HTTPServer, BaseHTTPRequestHandler
+
+# class MyHandler(BaseHTTPRequestHandler):
+#     pass
+
+
+# server = HTTPServer(("localhost", 8000), MyHandler)
+
+# print("Done")
+
+# server.serve_forever()
