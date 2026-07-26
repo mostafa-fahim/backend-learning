e845169663080b0c2f6c7ade284b4ab4dbe42bad
mostafa-fahim <mostafafahim280@gmail.com>
2026-07-26 13:44:02 +0600

    Improve server startup message

--- http/http-server/http_server.py
+++ http/http-server/http_server.py
@@ -1,15 +1,15 @@
-# from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-# class MyHandler(BaseHTTPRequestHandler):
-#     def do_GET(self):
-#         self.send_response(200)
-#         self.send_header("Content-Type", "text/plain")
-#         self.end_headers()
-#         self.wfile.write(b"Hello from my server")
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello from my server")
 
 
-# server = HTTPServer(("localhost", 8000), MyHandler)
+server = HTTPServer(("localhost", 8000), MyHandler)
 
-# print("Hello from my server")
+print("The server is running on http://localhost:8000")
 
-# server.serve_forever()
+server.serve_forever()
