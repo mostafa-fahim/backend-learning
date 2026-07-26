d7d71d53144a01d5c98c9628fbc6cb53c81cf913
mostafa-fahim <mostafafahim280@gmail.com>
2026-07-26 13:12:01 +0600

    Implement GET request handling

--- http/http-server/http_server.py
+++ http/http-server/http_server.py
@@ -1,11 +1,15 @@
 # from http.server import HTTPServer, BaseHTTPRequestHandler
 
 # class MyHandler(BaseHTTPRequestHandler):
-#     pass
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header("Content-Type", "text/plain")
+#         self.end_headers()
+#         self.wfile.write(b"Hello from my server")
 
 
 # server = HTTPServer(("localhost", 8000), MyHandler)
 
-# print("Done")
+# print("Hello from my server")
 
 # server.serve_forever()
