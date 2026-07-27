e1e37171e0ab36e6253ac579d86a03c20aa7e738
mostafa-fahim <mostafafahim280@gmail.com>
2026-07-27 15:33:56 +0600

    Refactor repetitive response code into send_text method

--- http/http-server/http_server.py
+++ http/http-server/http_server.py
@@ -2,24 +2,22 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
 class MyHandler(BaseHTTPRequestHandler):
+    def send_text(self, status_code, message):
+        self.send_response(status_code)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(message)
+
     def do_GET(self):
         if self.path == "/":
-            self.send_response(200)
-            self.send_header("Content-Type", "text/plain")
-            self.end_headers()
-            self.wfile.write(b"Home Page")
+            self.send_text(200, b"Home Page")
 
         elif self.path == "/users":
-            self.send_response(200)
-            self.send_header("Content-Type", "text/plain")
-            self.end_headers()
-            self.wfile.write(b"Users Page")
+            self.send_text(200, b"Users Page")
 
         else:
-            self.send_response(400)
-            self.send_header("Content-Type", "text/plain")
-            self.end_headers()
-            self.wfile.write(b"Page Not Found")
+            self.send_text(404, b"Page Not Found")
+
 
 server = HTTPServer(("localhost", 8000), MyHandler)
 
