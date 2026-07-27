24e17f76469a2e6cc15601671be4be7c0c20d615
mostafa-fahim <mostafafahim280@gmail.com>
2026-07-27 12:30:48 +0600

    Implement basic route handling

--- http/http-server/http_server.py
+++ http/http-server/http_server.py
@@ -1,15 +1,28 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Hello from my server")
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Home Page")
 
+        elif self.path == "/users":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Users Page")
+
+        else:
+            self.send_response(400)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Page Not Found")
 
 server = HTTPServer(("localhost", 8000), MyHandler)
 
-print("The server is running on http://localhost:8000")
+print("My server is running on http://localhost:8000")
 
 server.serve_forever()
