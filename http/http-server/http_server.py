eda69edcd9ef9ed2eca5200a0887478deff27470
mostafa-fahim <mostafafahim280@gmail.com>
2026-07-27 18:59:04 +0600

    Refactor response handling and add JSON support

--- http/http-server/http_server.py
+++ http/http-server/http_server.py
@@ -2,21 +2,22 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
 class MyHandler(BaseHTTPRequestHandler):
-    def send_text(self, status_code, message):
+
+    def send_text(self, status_code, content_type, message):
         self.send_response(status_code)
-        self.send_header("Content-Type", "text/plain")
+        self.send_header("Content-Type", content_type)
         self.end_headers()
         self.wfile.write(message)
 
     def do_GET(self):
         if self.path == "/":
-            self.send_text(200, b"Home Page")
+            self.send_text(200, "text/plain", b"Home Page")
 
         elif self.path == "/users":
-            self.send_text(200, b"Users Page")
+            self.send_text(200, "application/json", b'[{"name": "Karim", "age": 22}, {"name": "Ahmed", "age": 21}]')
 
         else:
-            self.send_text(404, b"Page Not Found")
+            self.send_text(404, "text/plain", b"Page Not Found")
 
 
 server = HTTPServer(("localhost", 8000), MyHandler)
